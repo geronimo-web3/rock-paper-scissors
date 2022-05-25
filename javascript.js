970e3d6d558387f0f619d62b85d8137cb44f5f0a
geronimo-web3 <web3@finsol.ch>
2022-05-25 12:16:31 +0200

    Playcode added to js

---
+++ javascript.js
@@ -0,0 +1,62 @@
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection){
+        return `No one wins! You have both ${playerSelection}`
+    }  else if (playerSelection === "rock" && computerSelection === "scissors"){
+        return `Player wins! Rock beats Scissors`
+    }
+        else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return `Player wins! Scissors beats Paper`
+    }
+        else if (playerSelection === "paper" && computerSelection === "rock"){
+        return `Player wins! Paper beats Rock`
+    }
+        else if (playerSelection === "scissors" && computerSelection === "rock"){
+        return `Computer wins! Rock beats Scissors`
+    }
+        else if (playerSelection === "paper" && computerSelection === "scissors"){
+        return `Computer wins! Scissors beats Paper`
+    }
+        else if (playerSelection === "rock" && computerSelection === "paper"){
+        return `Computer wins! Paper beats Rock`
+    }
+}
+  
+  let playerSelection;
+  let computerSelection;
+  game();
+
+  function game() {
+    for (let i = 0; i < 5; i++) {
+        playerSelection = playerPlay();
+        computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+  }
+
+  function playerPlay(){
+    let playerChoice;
+    
+    let playerPrompt = prompt("Make your choice: rock, paper or scissors");
+    playerChoice = playerPrompt.toLowerCase();
+    console.log(playerChoice);
+    
+    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors"){
+        return playerChoice;
+    } else{
+        playerPlay();
+    }
+  }
+
+  function computerPlay(){
+      const resultPlay = parseInt(Math.random()*3+1);
+      switch (resultPlay) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        default:
+            return "scissors"
+        }
+    }
