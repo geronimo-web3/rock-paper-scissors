331b870e43448897ef976b25feb7d845d8f234c0
geronimo-web3 <web3@finsol.ch>
2022-06-27 11:32:51 +0200

    added everthandler

--- javascript.js
+++ javascript.js
@@ -31,8 +31,42 @@ function playRound(playerSelection, computerSelection) {
   let computerSelection;
   let playerWins = 0;
   let computerWins = 0;
-//  game();
+  //game();
+
+  const finalResult = document.querySelector("#gameResult");
+  finalResult.innerHTML = "Final Result:";
   console.log(`Final result - Player: ${playerWins} // Computer: ${computerWins}`);
+
+  const playerSel = document.querySelector("#playerSel");
+  const computerSel = document.querySelector("#computerSel");
+
+  const choiceRock = document.querySelector("#myRock");
+  const choicePaper = document.querySelector("#myPaper");
+  const choiceScissors = document.querySelector("#myScissors");
+
+  choiceRock.addEventListener('click', () => {
+    playerSel.src = "images/rock.jpg";
+    playerSelection = "rock";
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    finalResult.innerHTML = `Result - Player: ${playerWins} // Computer: ${computerWins}`;
+  });
+
+  choicePaper.addEventListener('click', () => {
+    playerSel.src = "images/paper.jpg";
+    playerSelection = "paper";
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    finalResult.innerHTML = `Result - Player: ${playerWins} // Computer: ${computerWins}`;
+  });
+
+  choiceScissors.addEventListener('click', () => {
+    playerSel.src = "images/scissors.jpg";
+    playerSelection = "scissors";
+    computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    finalResult.innerHTML = `Result - Player: ${playerWins} // Computer: ${computerWins}`;
+ });
 
   function game() {
     for (let i = 0; i < 5; i++) {
@@ -45,15 +79,16 @@ function playRound(playerSelection, computerSelection) {
 
   function playerPlay(){
     let playerChoice;
-    
-//    let playerPrompt = prompt("Make your choice: rock, paper or scissors");
+ 
+ 
+  //    let playerPrompt = prompt("Make your choice: rock, paper or scissors");
 //    playerChoice = playerPrompt.toLowerCase();
     console.log(playerChoice);
     
     if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors"){
         return playerChoice;
     } else{
-        playerPlay();
+     //   playerPlay();
     }
   }
 
@@ -61,12 +96,15 @@ function playRound(playerSelection, computerSelection) {
       const resultPlay = parseInt(Math.random()*3+1);
       switch (resultPlay) {
         case 1:
+            computerSel.src = "images/rock.jpg";
             return "rock";
             break;
         case 2:
+            computerSel.src = "images/paper.jpg";
             return "paper";
             break;
         default:
+            computerSel.src = "images/scissors.jpg";
             return "scissors"
         }
     }
